540ba81a7669745d0581bd93d299490643c5e93e
tzahibd-netops <tsahibendavid+netops@gmail.com>
2024-11-19 12:40:17 -0700

    Update versions.tf

--- versions.tf
+++ versions.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     aviatrix = {
       source  = "AviatrixSystems/aviatrix"
-      version = "3.2.0"
+      version = "3.1.6"
     }
     aws = {
       source  = "hashicorp/aws"
